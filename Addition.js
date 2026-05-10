fcf5e958a101d57ce43bdc6c5ec6c0d6c02335e6
ahsan-fullstack <ahsanrehman728@gmail.com>
2026-05-10 16:58:57 +0500

    update the console

--- Addition.js
+++ Addition.js
@@ -1,6 +1,6 @@
 function subtraction(a, b) {
     console.log('subtraction logic');
-    console.log('This is no more conflict')
+    console.log('This is no more conflict','showing the message', a,b)
     return a - b;
 }
 
