6a2dfb19e02585371f1bd3e15b54dfc5d761cf8b
AhsanRehman12 <ahsanrehman599@gmail.com>
2026-05-10 16:58:10 +0500

    changing the message

--- Addition.js
+++ Addition.js
@@ -1,6 +1,6 @@
 function subtraction(a, b) {
     console.log('subtraction logic');
-    console.log('This is no more conflict')
+    console.log('This is no more conflict', a, b)
     return a - b;
 }
 
